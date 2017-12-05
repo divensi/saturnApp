a97b21d78d7a1fee464b5efb823f940e5b76a593
Felipe Divensi Eckl <feckl@frimesa.local>
2017-12-05 08:06:31 -0200

    Estilo atualizado, e pagina inicial alterada

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,1 +1,10 @@
 ﻿// Write your JavaScript code.
+//Main navigation scroll spy for shadow
+$(window).scroll(function () {
+    var y = $(window).scrollTop();
+    if (y > 0) {
+        $("#header").addClass('--not-top');
+    } else {
+        $("#header").removeClass('--not-top');
+    }
+});
